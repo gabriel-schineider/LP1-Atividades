4a3551fae07eb999e98deb267c6232a29faeb688
Gabriel Schineider <ogabrielschneider@gmail.com>
2025-12-08 15:56:12 -0300

    Atualizando exercicio3.c - (agora sim está funcionando como deveria)

--- Atividade_8_Arquivos-navegacao/exercicio3.c
+++ Atividade_8_Arquivos-navegacao/exercicio3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int tamanho (FILE * f);
 
 int main (void)
@@ -8,11 +7,19 @@ int main (void)
   // Usando o arquivo do exercício 1 como exemplo
 
   FILE* f = fopen("a.txt", "r+");
-  int meio = tamanho(f) / 2;
+  int total = tamanho(f);
+  int meio = total / 2;
+  int resto = total - meio;
 
   fseek(f, meio, SEEK_SET);
-
+  char buffer[resto]; // Construindo uma variável para armazenar tudo que está escrito do meio até o final do arquivo
+  fread(buffer, sizeof(char), resto, f);
+  
+  fseek(f, meio, SEEK_SET);
   fwrite("|meio|", sizeof(char), 6, f);
+  fwrite(buffer, sizeof(char), resto, f);
+
+  fclose(f);
 
   return 0;
 }
